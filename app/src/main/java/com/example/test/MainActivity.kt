e4efd0210d15800baea34a58ae156f53be957479
UPLukeet <up858296@myport.ac.uk>
2019-12-03 21:31:58 +0000

    fixed crashes and added non working code for BMI calculation, Hello world text stuck.

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -11,16 +11,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnChangeText=findViewById(R.id.btn_change_text)as Button;
-        val tvBMI=findViewById(R.id.output_BMI) as TextView;
-        var weight=findViewById(R.id.input_weight) as EditText;
-        var height=findViewById(R.id.input_height) as EditText;
-        var bmiCalc=(height.text.toString().toInt()+weight.text.toString().toInt());
+        val btnChangeText = findViewById(R.id.btn_change_text) as Button;
+        val tvBMI = findViewById(R.id.output_BMI) as TextView;
+        val weight = findViewById(R.id.input_weight) as EditText;
+        val height = findViewById(R.id.input_height) as EditText;
+        var bmiWeight = 0;
+        var bmiHeight = 0;
+        var bmiCalc = 0;
 
         btnChangeText.setOnClickListener {
-            tvBMI.text= bmiCalc.toString();
+            bmiWeight = (weight.text.toString().toInt());
+            bmiHeight = (height.text.toString().toInt());
+            bmiCalc = (bmiWeight / bmiHeight / bmiHeight) * 10000;
+            tvBMI.text = bmiCalc.toString();
         }
 
     }
 
-    }
+}

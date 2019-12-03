31345ed4cccd601cf1f1e85c2c5e2eeb60e60839
UPLukeet <up858296@myport.ac.uk>
2019-12-03 22:21:41 +0000

    Cleaning up kotlin BMI code, still not working.

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -11,19 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnChangeText = findViewById(R.id.btn_change_text) as Button;
-        val tvBMI = findViewById(R.id.output_BMI) as TextView;
-        val weight = findViewById(R.id.input_weight) as EditText;
-        val height = findViewById(R.id.input_height) as EditText;
-        var bmiWeight = 0;
-        var bmiHeight = 0;
-        var bmiCalc = 0;
+        val btnChangeText = findViewById(R.id.btn_change_text) as Button
+        val tvBMI = findViewById(R.id.output_BMI) as TextView
+        val weight = findViewById(R.id.input_weight) as EditText
+        val height = findViewById(R.id.input_height) as EditText
 
         btnChangeText.setOnClickListener {
-            bmiWeight = (weight.text.toString().toInt());
-            bmiHeight = (height.text.toString().toInt());
-            bmiCalc = (bmiWeight / bmiHeight / bmiHeight) * 10000;
-            tvBMI.text = bmiCalc.toString();
+           val bmiWeight = weight.text.toString().toInt()
+           val bmiHeight = height.text.toString().toInt()
+           val bmiCalc =  bmiWeight /  bmiHeight /  bmiHeight * 10000
+            tvBMI.text = bmiCalc.toString()
         }
 
     }
